72b4a8f401868147add6fd5e11fe0e002c69852f
Piyush <piyusharyaprakash12345@gmail.com>
2022-12-26 16:01:11 +0530

    My portfolio webiste 1st commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var options = {
+    strings: [
+      ,
+      "I am a <span>Programmer</span>",
+      "I am a <span>Developer</span>",
+      "I am a <span>Coder</span>",
+    ],
+    typeSpeed: 40,
+    backspeed: 20,
+    showCursor: true,
+    loop: true,
+  };
+  
+  var typed = new Typed("#typed", options);
+  
