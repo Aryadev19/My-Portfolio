78578b8a575e337310d4fe04a47b65833354fb39
Piyush <piyusharyaprakash12345@gmail.com>
2023-02-01 19:52:29 +0530

    projects links are now working

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ var options = {
   
   var typed = new Typed("#typed", options);
   
+  const p1 = document.querySelector(".p1");
+  const p2 = document.querySelector(".p2");
+  const p3 = document.querySelector(".p3");
+
+  p1.onclick = ()=>{
+    window.location.href="http://roasted-potato.vercel.app/"
+  }
+
+  p2.onclick = ()=>{
+    window.location.href="http://pok-dex-blush.vercel.app/";
+  };
+
+  p3.onclick = ()=>{
+    window.location.href="https://factomania.vercel.app/";
+  }
